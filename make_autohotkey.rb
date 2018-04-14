d8bd34111e8d54ab1f5ec84351c9c023c2796c9b
Schwark Satyavolu <schwark@alum.rpi.edu>
2018-04-13 22:44:59 -0700

    adding autohotkey

---
+++ make_autohotkey.rb
@@ -0,0 +1,75 @@
+#!/usr/bin/ruby
+
+# Adapted from https://github.com/noahmanger/emoji-text-replacement/
+
+require 'erb'
+require 'rubygems'
+require 'json'
+
+def get_template()
+  %{
+<% for @item in @items %>
+::<%= @item['code'] %>::<%=h @item['unicode'] %>\r
+<% end %>
+  }
+end
+
+def get_yosemite_template()
+  %{
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <array>
+        <% for @item in @items %>
+        <dict>
+          <key>phrase</key>
+          <string><%= @item['unicode'] %></string>
+          <key>shortcut</key>
+          <string><%=h @item['code'] %></string>
+        </dict>
+      <% end %>
+      </array>
+    </plist>
+  }
+end
+
+def get_items(filename)
+  replace_pairs = []
+
+  file = File.read(filename)
+  replacements = JSON.parse(file)
+
+  replacements.each do |code, value|
+    unicode = value['replacement']
+    replace_pairs << {'code' => code, 'unicode' => unicode}
+  end
+
+  return replace_pairs
+end
+
+class TextReplaceList
+  include ERB::Util
+  attr_accessor :items, :template
+
+  def initialize(items, template)
+    @items = items
+    @template = template
+  end
+
+  def render()
+    ERB.new(@template, 0, '>').result(binding)
+  end
+
+  def save(file)
+    File.open(file, "w+") do |f|
+      f.write(render)
+    end
+  end
+
+end
+
+Dir.entries(".").each do |filename|
+  next if not filename.end_with?(".json")
+  list = TextReplaceList.new(get_items(filename), get_template)
+  list.save(File.join(File.dirname(__FILE__), filename+'.ahk'))
+end
---
+++ telugu.json.ahk
@@ -0,0 +1,13 @@
+﻿
+::ela::ఎలా
+::endaro::ఎందరో
+::chesava::చేసావా
+::chestanu::చేస్తాను
+::chestunanu::చేస్తున్నాను
+::mahanubhavulu::మహానుభావులు
+::naku::నాకు
+::nenu::నేను
+::neeku::నీకు
+::nuvvu::నువ్వు
+::undi::ఉంది
+  
